21264e2c88081882801c6d032e27cbfe91413ce8
Olexii Kononenko <kononenkoa995@gmail.com>
2017-11-17 13:48:49 +0200

    Added submit project and registration.

---
+++ src/main/java/com/controller/command/impl/SubmitProjectSettings.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wookie.devteam.controller.command.impl;
+
+import com.wookie.devteam.constants.Attributes;
+import com.wookie.devteam.controller.Controller;
+import com.wookie.devteam.controller.command.Command;
+import com.wookie.devteam.controller.command.impl.extractor.Extractor;
+import com.wookie.devteam.entities.Person;
+import com.wookie.devteam.service.PersonProjectService;
+import com.wookie.devteam.service.PersonService;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Method submits all setting made by manager to the project.
+ * 
+ * <p>Method redirects user back to his main page.
+ * @author wookie
+ */
+public class SubmitProjectSettings extends Command {
+    
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException, RuntimeException {
+    
+        int personId = (int) request.getSession().getAttribute(Attributes.USER_SESSION);
+        String personRole = (String) request.getSession().getAttribute(Attributes.USER_ROLE_SESSION);
+        
+        serviceFactory.getPersonProjectService().createPersonProjectFromRequest(
+                extractor.extractIntSet(request, Attributes.SELECT_DEVS),
+                personId,
+                extractor.extractInt(request, Attributes.PROJECT_ID),
+                extractor.extractBigDecimal(request, Attributes.PROJECT_COST)
+        );
+        
+        return serviceFactory.getPersonService().processUser(request, personId, personRole);
+    }
+    
+}
---
+++ src/main/java/com/controller/command/impl/SubmitRegistrationCommand.java
@@ -0,0 +1,39 @@
+package com.wookie.devteam.controller.command.impl;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+
+import com.wookie.devteam.constants.Attributes;
+import com.wookie.devteam.constants.WebPages;
+import com.wookie.devteam.controller.Controller;
+import com.wookie.devteam.controller.command.Command;
+import com.wookie.devteam.controller.command.impl.extractor.Extractor;
+import com.wookie.devteam.service.PersonService;
+
+public class SubmitRegistrationCommand extends Command {
+	private static final org.apache.logging.log4j.Logger logger = LogManager.getLogger(SubmitRegistrationCommand.class);
+    
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException, RuntimeException {
+		
+		logger.info("Start creating new person");
+		serviceFactory.getPersonService().createPerson(
+				extractor.extractString(request, Attributes.USER_NAME),
+				extractor.extractString(request, Attributes.USER_SURNAME), 
+				extractor.extractString(request, Attributes.USER_LOGIN),
+				extractor.extractEmail(request, Attributes.USER_EMAIL), 
+				extractor.extractPhoneNumber(request, Attributes.USER_PHONE), 
+				extractor.extractPastDate(request, Attributes.USER_BIRTHDAY), 
+				extractor.extractString(request, Attributes.USER_PASSWORD)
+		);
+		
+		return WebPages.LOGIN_PAGE;
+	}
+
+}
