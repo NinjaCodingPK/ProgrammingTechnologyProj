d49315d609b452cc8f47c34f6a5692cf10340991
Eugenia Dzytsiuk <eugeniadzytsiuk@ukr.net>
2017-12-26 14:52:46 +0200

    Update DaoFactory.java

--- src/main/java/com/controller/dao/DaoFactory.java
+++ src/main/java/com/controller/dao/DaoFactory.java
@@ -1,9 +1,9 @@
-/*
+a/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.controller.dao;
+package com.dao;
 
 import java.sql.Connection;
 import javax.sql.DataSource;
